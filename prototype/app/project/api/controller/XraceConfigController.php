a473f9dd13879028842b56519cdac50d7161b4b7
zhoutai <zhoutai@b5m.com>
2016-02-23 00:26:19 +0800

    apij数据接口

--- prototype/app/project/api/controller/XraceConfigController.php
+++ prototype/app/project/api/controller/XraceConfigController.php
@@ -24,7 +24,7 @@ class XraceConfigController extends AbstractController
     }
     
     /**
-     *获得所有的RaceCatalogList
+     *获取所有赛事的列表
      */
     public function getRaceCatalogListAction()
     {
@@ -38,13 +38,78 @@ class XraceConfigController extends AbstractController
     }
     
     /*
-     * 获得单条RaceCatalog
+     * 获取单个赛事信息
      */
     public function getRaceCatalogAction() {
         $RaceCatalogId = isset($this->request->RaceCatalogId)?intval($this->request->RaceCatalogId):0;
         $raceCatalog = $this->oRace->getRaceCatalog($RaceCatalogId);
-        
+        $raceCatalog['comment'] = json_decode($raceCatalog['comment'],true);
+        $result = array("return"=>0,"raceCatalog"=>$raceCatalog);
         echo json_encode($result);       
+    }
+    
+    /*
+     * 根据赛事获取所有分站列表
+     */
+    public function getRaceStageListAction() {
+        $RaceCatalogId = isset($this->request->RaceCatalogId)?intval($this->request->RaceCatalogId):0;
+        $raceStageList = $this->oRace->getAllRaceStageList($RaceCatalogId);
+//        foreach ($raceStageList as $key => $value) {
+//            $raceStageList[$key]['comment'] = json_decode($value['comment'],true);
+//        }
+        $result = array("return"=>0,"raceStageList"=>$raceStageList);
+        echo json_encode($result);
+    }
+    
+    /*
+     * 获取单个赛事分站信息
+     */    
+    public function getRaceStageAction() {
+        $RaceStageId = isset($this->request->RaceStageId)?intval($this->request->RaceStageId):0;
+        $raceStage = $this->oRace->getRaceStage($RaceStageId);
+        $result = array("return"=>0,"raceStage"=>$raceStage);
+        echo json_encode($result);
+    }
+    
+    /*
+     * 根据赛事获取所有赛事组别列表
+     */
+    public function getAllRaceGroupListAction() {
+        $RaceCatalogId = isset($this->request->RaceCatalogId)?intval($this->request->RaceCatalogId):0;
+        $raceGroupList = $this->oRace->getAllRaceGroupList($RaceCatalogId);
+        $result = array("return"=>0,"raceGroupList"=>$raceGroupList);
+        echo json_encode($result);
+        
+    }
+    /*
+     * 获取单个赛事组别的信息
+     */
+    public function getRaceGroupAction() {
+        $RaceGroupId = isset($this->request->RaceGroupId)?intval($this->request->RaceGroupId):0;
+        $raceGroup = $this->oRace->getRaceGroup($RaceGroupId);
+        $result = array("return"=>0,"raceGroup"=>$raceGroup);
+        echo json_encode($result);
+    }
+    /*
+     * 获取赛事分站和赛事组别获取比赛列表
+     */
+    public function getRaceListAction() {
+        $RaceStageId = isset($this->request->RaceStageId)?intval($this->request->RaceStageId):0;
+        $RaceGroupId = isset($this->request->RaceGroupId)?intval($this->request->RaceGroupId):0;
+        $raceList = $this->oRace->getRaceList($RaceStageId, $RaceGroupId);
+        $result = array("return"=>0,"raceList"=>$raceList);
+        echo json_encode($result);
+        
+    }
+    
+    /*
+     * 获得单个比赛信息
+     */
+    public function getRaceAction() {
+        $RaceId = isset($this->request->RaceId)?intval($this->request->RaceId):0;
+        $race = $this->oRace->getRaceInfo($RaceId);
+        $result = array("return"=>0,"race"=>$race);
+        echo json_encode($result); 
     }
 
 }
