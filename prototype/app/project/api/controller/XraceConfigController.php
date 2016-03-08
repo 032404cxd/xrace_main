df6334a34ae3ccc39a46f51d0337c8ac0e0b7e35
cxd <cxd032404@hotmail.com>
2016-03-08 22:18:54 +0800

    merge

--- prototype/app/project/api/controller/XraceConfigController.php
+++ prototype/app/project/api/controller/XraceConfigController.php
@@ -197,7 +197,6 @@ class XraceConfigController extends AbstractController
             //全部置为空
             $result = array("return"=>0,"RaceStageInfo"=>array(),'RaceGroupList'=>array(),"comment"=>"请指定一个有效的分站ID");
         }
-        print_R($result);
         echo json_encode($result);
     }
     /*
