1ef1c0618d829d7f9a8e032a55820ed175f1f46f
tieji <tieji@b5m.com>
2016-03-24 20:05:13 +0800

    sql

--- prototype/app/doc/app_os.sql
+++ prototype/app/doc/app_os.sql
@@ -1,6 +1,3 @@
-
--- --------------------------------------------------------
-
 --
 -- 表的结构 `config_app_os`
 --
@@ -17,8 +14,8 @@ CREATE TABLE IF NOT EXISTS `config_app_os` (
 --
 
 INSERT INTO `config_app_os` (`AppOSId`, `AppOSName`, `comment`) VALUES
-(1, 'IOS2', ''),
-(2, 'abc', '');
+(1, 'IOS', ''),
+(2, 'ANDROID', '');
 
 -- --------------------------------------------------------
 
@@ -50,17 +47,22 @@ INSERT INTO `config_app_type` (`AppTypeId`, `AppTypeName`, `comment`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `config_app_version` (
-  `VersionId` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-  `AppVersion` varchar(32) COLLATE utf8_bin NOT NULL COMMENT 'APP版本',
-  `AppType` int(4) unsigned NOT NULL COMMENT 'APP系统类型',
-  `comment` varchar(1024) COLLATE utf8_bin NOT NULL COMMENT '压缩数组',
-  PRIMARY KEY (`VersionId`),
-  KEY `AppType` (`AppType`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='APP版本管理' AUTO_INCREMENT=1 ;
+  `AppVersionId` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `AppVersion` varchar(32) CHARACTER SET utf8 NOT NULL COMMENT 'APP版本',
+  `AppTypeId` int(4) unsigned NOT NULL COMMENT 'APP系统类型',
+  `AppOSId` int(4) unsigned NOT NULL COMMENT 'APP系统类型',
+  `AppDownloadUrl` varchar(256) CHARACTER SET utf8 NOT NULL COMMENT 'APP下载路径',
+  `comment` varchar(1024) CHARACTER SET utf8 NOT NULL COMMENT '压缩数组',
+  PRIMARY KEY (`AppVersionId`),
+  UNIQUE KEY `UniqueVersion` (`AppVersion`,`AppTypeId`,`AppOSId`),
+  KEY `AppType` (`AppTypeId`),
+  KEY `AppVersion` (`AppVersion`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='APP版本管理' AUTO_INCREMENT=5 ;
 
-ALTER TABLE `config_app_version` ADD `AppOSId` INT( 4 ) UNSIGNED NOT NULL COMMENT 'APP系统类型' AFTER `AppType` ;
-ALTER TABLE `config_app_version` CHANGE `AppType` `AppTypeId` INT( 4 ) UNSIGNED NOT NULL COMMENT 'APP系统类型';
-ALTER TABLE `config_app_version` CHANGE `AppVersion` `AppVersion` VARCHAR( 32 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'APP版本';
-ALTER TABLE `config_app_version` CHANGE `comment` `comment` VARCHAR( 1024 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '压缩数组';
-ALTER TABLE `config_app_version` CHANGE `VersionId` `AppVersionId` INT( 10 ) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键ID';
-ALTER TABLE `config_app_version` ADD `AppDownloadUrl` VARCHAR( 256 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'APP下载路径' AFTER `AppOSId` ;
+--
+-- 转存表中的数据 `config_app_version`
+--
+
+INSERT INTO `config_app_version` (`AppVersionId`, `AppVersion`, `AppTypeId`, `AppOSId`, `AppDownloadUrl`, `comment`) VALUES
+(3, '1.2.1', 1, 2, 'asdfasgsadgasdg', '{"VersionComment":"1.\\u7248\\u672c\\u8bf4\\u660e\\n2.\\u7248\\u672c\\u8bf4\\u660e2\\n3.\\u7248\\u672c\\u8bf4\\u660e3"}'),
+(4, '1.2.2', 2, 2, 'adfasfdasdgsaga111', '{"VersionComment":"asgaggsgags111"}');
