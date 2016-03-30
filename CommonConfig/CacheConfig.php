9a6a700f83f0d9fd4a0c5cca0fb0d1fa35eb6074
tieji <tieji@b5m.com>
2016-03-30 16:44:15 +0800

    merge

--- CommonConfig/CacheConfig.php
+++ CommonConfig/CacheConfig.php
@@ -1,8 +1,8 @@
 <?php
 return array
 (
-	'MEMECACHE_SERVER' => array('192.168.130.189'),
-	'MEMECACHE_PORT'   => '12000',
+	'MEMECACHE_SERVER' => array('memcache.xrace.cn'),
+	'MEMECACHE_PORT'   => '59999',
 	
 	'REDIS_SERVER' => array('127.0.0.1'),
 	'REDIS_PORT'   => '6379',
