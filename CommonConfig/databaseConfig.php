ce5bea4ac7d93e6a5f18531fb8afb4810e46a545
cxd <cxd032404@hotmail.com>
2016-04-26 07:10:34 +0800

    merge

--- CommonConfig/databaseConfig.php
+++ CommonConfig/databaseConfig.php
@@ -1,7 +1,10 @@
 <?php
+
+
+
 define(HOST_LOCAL,'localhost');
-define(PORT_LOCAL,3306);
-define(USER_LOCAL,'root');
-define(PASSWORD_LOCAL,'');
+define(PORT_LOCAL,3307);
+define(USER_LOCAL,'dbo16');
+define(PASSWORD_LOCAL,'xrAec95R');
 
 ?>
