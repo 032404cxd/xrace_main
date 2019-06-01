278730563b0ac6c580d5a50cbac22891aafb71f9
cxd <cxd032404@hotmail.com>
2019-06-01 11:41:52 +0800

    配置更新

--- CommonConfig/CacheConfig.php
+++ CommonConfig/CacheConfig.php
@@ -1,8 +1,12 @@
 <?php
 return array
 (
-	'MEMECACHE_SERVER' => array('memcache.xrace.cn'),
-	'MEMECACHE_PORT'   => '59999',
+//	'MEMECACHE_SERVER' => array('memcache.xrace.cn'),
+	'MEMECACHE_SERVER' => array('127.0.0.1'),
+
+	'MEMECACHE_PORT'   => '39767',
+//	'MEMECACHE_PORT'   => '11211',
+
 	
 	'REDIS_SERVER' => array('127.0.0.1'),
 	'REDIS_PORT'   => '6379',
--- CommonConfig/databaseConfigOnline.php
+++ CommonConfig/databaseConfigOnline.php
@@ -3,4 +3,7 @@ define(HOST_LOCAL,'rdsqfd12mpc805g02650.mysql.rds.aliyuncs.com');
 define(PORT_LOCAL,3306);
 define(USER_LOCAL,'dbo16');
 define(PASSWORD_LOCAL,'xrAec95R');
+
+define(USER_LOCAL_RC,'rcdbuser');
+define(PASSWORD_LOCAL_RC,'Sep17MyYgL');
 ?>
