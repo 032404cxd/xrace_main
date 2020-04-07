3f66747b145db7f76c5f8b0f6971f047914f1c3a
justin.chen <cxd032404@hotmail.com>
2020-04-07 22:59:30 +0800

    merge

--- CommonConfig/databaseConfigLocal.php
+++ CommonConfig/databaseConfigLocal.php
@@ -1,18 +1,16 @@
 
 <?php
-/*
-define(HOST_LOCAL,'localhost');
+
+define(HOST_LOCAL,'127.0.0.1');
 define(PORT_LOCAL,3307);
 define(USER_LOCAL,'dbo16');
 define(PASSWORD_LOCAL,'xrAec95R');
-*/
+/*
 define(HOST_LOCAL,'127.0.0.1');
 define(PORT_LOCAL,3306);
 define(USER_LOCAL,'root');
 define(PASSWORD_LOCAL,'');
-
-define(USER_LOCAL_RC,'root');
-define(PASSWORD_LOCAL_RC,'');
+*/
 
 
 
